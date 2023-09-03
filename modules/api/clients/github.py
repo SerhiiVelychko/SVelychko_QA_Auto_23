1d1b483b6912e71f44c930dfc73475a4c936c552
Serhii Velychko <velychko2019@ukr.net>
2023-09-03 18:43:11 +0300

    Final task

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -15,3 +15,34 @@ class GitHub:
         body = r.json()
 
         return body
+
+    def get_emojis(self):
+        r = requests.get("https://api.github.com/emojis")
+        body = r.json()
+
+        return body
+
+    def get_emoji_image_url(self, emoji_name):
+        emojis = self.get_emojis()
+        if emoji_name in emojis:
+            return emojis[emoji_name]
+        return None
+
+    def get_non_existing_emoji_info(self, emoji_name):
+        r = requests.get("https://api.github.com/emojis/{emoji_name}")
+        return r
+
+    def get_commit(self, owner, repo):
+        r = requests.get(
+            "https://api.github.com/repos/{owner}/{repo}/commits",
+            params={"owner": owner, "repo": repo},
+        )
+        body = r.json()
+
+        return body
+
+    def get_latest_commits(self, owner, repo):
+        r = requests.get("https://api.github.com/repos/{owner}/{repo}/commits")
+        body = r.json()
+
+        return body
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -30,3 +30,38 @@ def test_repo_cannot_be_found(github_api):
 def test_repo_with_single_char_be_found(github_api):
     r = github_api.search_repo("s")
     assert r["total_count"] != 0
+
+
+@pytest.mark.api
+def test_get_emojis(github_api):
+    r = github_api.get_emojis()
+    print(r)
+
+
+@pytest.mark.api
+def test_get_emoji_image_url(github_api):
+    emoji_name = "heart"
+    image_url = github_api.get_emoji_image_url(emoji_name)
+
+    assert image_url is not None
+    assert (
+        "https://github.githubassets.com/images/icons/emoji/unicode/2764.png"
+        in image_url
+    )
+
+
+@pytest.mark.api
+def test_get_non_existing_emoji_info(github_api):
+    non_existing_emoji_name = "non_existing_emoji"
+    r = github_api.get_non_existing_emoji_info(non_existing_emoji_name)
+
+    assert r.status_code == 404
+    assert "Not Found" in r.text
+
+
+@pytest.mark.api
+def test_get_latest_commits(github_api):
+    owner = "SerhiiVelychko"
+    repo = "SVelychko_QA_Auto_23"
+    r = github_api.get_latest_commits(owner, repo)
+    print(r)
